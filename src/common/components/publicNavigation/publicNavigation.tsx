d66007cecc666d6d78779223e654db39ce45ed90
eljosema505 <eljosema505@gmail.com>
2024-07-26 09:52:56 -0400

    feat: Test the ci/cd

--- src/common/components/publicNavigation/publicNavigation.tsx
+++ src/common/components/publicNavigation/publicNavigation.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const PublicNavigation = () => {
   return (
     <>
-      <div>navbar public</div>
+      <div>navbar public test</div>
       <Outlet />
     </>
   );
